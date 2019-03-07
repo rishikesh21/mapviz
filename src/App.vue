13b3bb01d1d68e8380c7cb093f63db573cced64f
Rishikeshan RAJENDRAM <rishikeshanr@smu.edu.sg>
2019-03-07 12:09:28 +0800

    Updated the map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.4/vue.min.js"></script>
+<script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+<div class="container mt-3 mt-sm-5" id="app">
+  <div class="row">
+    <div class="col-md-9">
+      <div class="map" id="map"></div>
+    </div>
+    <div class="col-md-3">
+      <div
+        class="form-check"
+        v-for="layer in layers"
+        :key="layer.id"
+      >
+        <label class="form-check-label">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="layer.active"
+            @change="layerChanged(layer.id, layer.active)"
+          />
+          {{ layer.name }}
+        </label>
+      </div>
+    </div>
   </div>
+</div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
